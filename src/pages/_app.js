98c41e1fbe758ab2455051223d588b29a8c86ebf
Stepan72 <st.nim@list.ru>
2023-04-20 10:17:23 +0600

    version 0.1 Home start

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import React from "react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
