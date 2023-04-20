911750c161aee021094a4cc7a222c33d68cbed9d
Stepan72 <st.nim@list.ru>
2023-04-20 15:13:09 +0600

    version 0.2 banner start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  Cart,
+  Footer,
+  FooterBanner,
+  HeroBanner,
+  Layout,
+  Navbar,
+  Product,
+} from "@/components";
+import { client } from "../../lib/client";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
+  // console.log(products);
+  // console.log(bannerData);
+
   return (
     <div>
-      <>HeroBanner</>
+      <>
+        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      </>
       <div className="products-heading">
         <h2>Best Selling Products</h2>
-        <p>Speakers of many variatoons</p>
+        <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product, index) => {
-          return product;
+        {products?.map((product, index) => {
+          return product.name;
         })}
       </div>
-      Footer
+      <FooterBanner />
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productsQuery = "*[_type == 'product']";
+  const products = await client.fetch(productsQuery);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
