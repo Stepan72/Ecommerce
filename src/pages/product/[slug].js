8bfa2db557a928bc1cde5d34267586a22b20c542
Stepan72 <st.nim@list.ru>
2023-04-22 21:51:03 +0600

    detail-product finished

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { client, urlFor } from "../../../lib/client";
 import {
   AiOutlineMinus,
@@ -6,8 +6,11 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+import { Product } from "@/components";
 
 const ProductDetails = ({ product, products }) => {
+  const [index, setIndex] = useState(0);
+
   const { image, name, details, price } = product;
 
   return (
@@ -15,13 +18,27 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* <div className="small-inmages-container">
-            {image?.map((item, index) => {
-              return <img src={urlFor(item)} key={index} className="" onMouseEnter="" />;
+          <div className="small-images-container">
+            {image?.map((item, i) => {
+              return (
+                <img
+                  src={urlFor(item)}
+                  key={i}
+                  className={
+                    i === index ? "small-image selected-image" : "small-image"
+                  }
+                  onMouseEnter={() => {
+                    setIndex(i);
+                  }}
+                />
+              );
             })}
-          </div> */}
+          </div>
           <div className="product-detail-desc">
             <h1>{name}</h1>
             <div className="reviews">
@@ -59,6 +76,16 @@ const ProductDetails = ({ product, products }) => {
                 Buy Now
               </button>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item, index) => {
+              return <Product key={item._id} product={item} />;
+            })}
           </div>
         </div>
       </div>
